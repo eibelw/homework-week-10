bca3ff28ee748b72119b9dc238cb458c7646e6c6
Abel <abelwinson15@gmail.com>
2023-10-14 20:27:38 +0700

    disesuaikan dengan fitur baru

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const MoviesController = require('../controller/moviesController')
+const express = require("express");
+const router = express.Router();
+const MoviesController = require("../controller/moviesController");
 
-router.get('/', MoviesController.index)
-// router.get('/', MoviesController.index)
-// router.get('/', MoviesController.index)
-// router.get('/', MoviesController.index)
-// router.get('/', MoviesController.index)
+router.get("/", MoviesController.index);
+router.get("/:id", MoviesController.show);
+router.post("/", MoviesController.create);
+router.put('/', MoviesController.update)
+router.delete('/:id', MoviesController.delete)
 
-module.exports = router
+module.exports = router;
