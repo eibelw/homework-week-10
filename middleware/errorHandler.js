d0c18b5ab22a574e147c8c19e12bdb79325dd79d
Abel <abelwinson15@gmail.com>
2023-10-14 20:54:15 +0700

    tambahan errorHandler untuk app

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -3,6 +3,8 @@ const errorHandler = (err, req, res, next) => {
 
   if (err.name == "paramsError") {
     res.status(400).json({ message: "Bad request" });
+  } else if (err.name == "Not found") {
+    res.status(404).json({ message: "Movie not found" });
   } else {
     res.status(500).json({ message: "Internal server error" });
   }
